5f93fad7b457a4ec9f3ce987632d6bac34539c68
Master Chit Chit <aztawong@aliyun.com>
2023-02-23 13:38:57 +0000

    update and delete

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import Koa from "koa";
-//import Router, {RouterContext} from "koa-router";
+import /*Router, */ { RouterContext } from "koa-router";
 import logger from "koa-logger";
 import json from "koa-json";
 
-import {router as articles} from "./routes/articles";
+import { router as articles } from "./routes/articles";
 
 const app: Koa = new Koa();
 //const router: Router = new Router();
@@ -19,6 +19,16 @@ app.use(logger());
 app.use(json());
 //app.use(router.routes());
 app.use(articles.routes());
-
+app.use(async (ctx: RouterContext, next: any) => {
+  try {
+    await next();
+    if(ctx.status === 404){
+      ctx.body = {err: "Resource not found"};
+    }
+  } catch(err: any) {
+    ctx.body = {err: err};
+  }
+  
+})
 
 app.listen(10888);
